b72af273da1f90c6a9509d8a479616de6193e235
Harsh Shandilya <me@msfjarvis.dev>
2024-09-23 12:35:16 +0530

    fix: replace `qr2term` with `fast_qr`
    Fixes rendering on macOS as well as significantly cuts down dependency tree

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use std::env;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let hostname = env::var("HOSTNAME").unwrap_or("localhost".to_string());
     let password = crate::password::generate_password();
-    qr2term::print_qr(format!("WIFI:T:ADB;S:ADBear@{hostname};P:{password};;"))
-        .map_err(|e| e.into())
+    fast_qr::QRBuilder::new(format!("WIFI:T:ADB;S:ADBear@{hostname};P:{password};;"))
+        .build()
+        .expect("Failed to print QR code")
+        .print();
+    Ok(())
 }
