61c81d982f6697c6252a4c46c3a9130265635ee2
Harsh Shandilya <me@msfjarvis.dev>
2024-09-26 12:46:21 +0530

    refactor(scanning): commonize service locating code

--- src/scanning/mod.rs
+++ src/scanning/mod.rs
@@ -6,29 +6,47 @@ use tokio::time::timeout;
 const MDNS_SCAN_TYPE: &str = "_adb-tls-connect._tcp.local.";
 const MDNS_PAIRING_TYPE: &str = "_adb-tls-pairing._tcp.local.";
 
-pub async fn find_pairing_service(identifier: &str) -> anyhow::Result<ServiceInfo> {
-    async fn inner(
-        mdns: &ServiceDaemon,
-        service_type: &str,
-        identifier: &str,
-    ) -> Option<ServiceInfo> {
-        let receiver = mdns.browse(service_type).expect("Failed to browse");
-        let service_name = format!("{identifier}.{MDNS_PAIRING_TYPE}");
+enum ServiceNameMatcher {
+    Exact,
+    Suffix,
+}
 
-        while let Ok(event) = receiver.recv_async().await {
-            if let ServiceEvent::ServiceResolved(info) = event {
-                if info.get_fullname() == service_name {
-                    return Some(info);
+async fn find_mdns_service(
+    mdns: &ServiceDaemon,
+    service_type: &str,
+    matcher: ServiceNameMatcher,
+) -> Option<ServiceInfo> {
+    let receiver = mdns.browse(service_type).expect("Failed to browse");
+
+    while let Ok(event) = receiver.recv_async().await {
+        if let ServiceEvent::ServiceResolved(info) = event {
+            match matcher {
+                ServiceNameMatcher::Exact => {
+                    if info.get_fullname() == service_type {
+                        return Some(info);
+                    }
+                }
+                ServiceNameMatcher::Suffix => {
+                    if info.get_fullname().ends_with(service_type) {
+                        return Some(info);
+                    }
                 }
             }
         }
-        None
     }
+    None
+}
+
+pub async fn find_pairing_service(identifier: &str) -> anyhow::Result<ServiceInfo> {
     let mdns = ServiceDaemon::new().expect("Failed to create daemon");
 
     match timeout(
         Duration::from_secs(30),
-        inner(&mdns, MDNS_PAIRING_TYPE, identifier),
+        find_mdns_service(
+            &mdns,
+            &format!("{identifier}.{MDNS_PAIRING_TYPE}"),
+            ServiceNameMatcher::Exact,
+        ),
     )
     .await
     {
@@ -48,22 +66,14 @@ pub async fn find_pairing_service(identifier: &str) -> anyhow::Result<ServiceInf
 }
 
 pub async fn find_connection_service() -> anyhow::Result<ServiceInfo> {
-    async fn inner(mdns: &ServiceDaemon, service_type: &str) -> Option<ServiceInfo> {
-        let receiver = mdns.browse(service_type).expect("Failed to browse");
-
-        while let Ok(event) = receiver.recv_async().await {
-            if let ServiceEvent::ServiceResolved(info) = event {
-                if info.get_fullname().ends_with(service_type) {
-                    return Some(info);
-                }
-            }
-        }
-        None
-    }
-
     let mdns = ServiceDaemon::new().expect("Failed to create daemon");
 
-    match timeout(Duration::from_secs(30), inner(&mdns, MDNS_SCAN_TYPE)).await {
+    match timeout(
+        Duration::from_secs(30),
+        find_mdns_service(&mdns, MDNS_SCAN_TYPE, ServiceNameMatcher::Suffix),
+    )
+    .await
+    {
         Ok(Some(info)) => {
             mdns.shutdown().unwrap();
             Ok(info)
